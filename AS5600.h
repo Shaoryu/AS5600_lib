cd51358a232c25d6a779f6770ef07306bdec2434
Shaoryu <149553583+Shaoryu@users.noreply.github.com>
2025-08-02 10:20:38 +0900

    返り値の変更

--- AS5600.h
+++ AS5600.h
@@ -6,14 +6,16 @@ class AS5600 {
 public:
     AS5600(I2C &i2c, PinName dirPin = NC);
 
+    
+
     // 初期化
     bool begin();
 
     // 生の角度（RAW_ANGLE）
-    uint16_t readRawAngle();
+    float readRawAngle();
 
     // スケーリングされた角度（ANGLE）
-    uint16_t readScaledAngle();
+    float readScaledAngle();
 
     // 接続されているか確認（MAGNET DETECTED）
     bool isMagnetDetected();
@@ -24,9 +26,14 @@ public:
     // DIRピン設定
     void setDirection(bool clockwise);
 
+    uint16_t read16(char reg);
+    uint8_t read8(char reg);
+
 private:
     I2C &_i2c;
-    DigitalOut *_dir;
+
+    DigitalOut _dir;
+    PinName _dirPin;
 
     static constexpr int AS5600_ADDR = 0x36 << 1; // 7bit address left-shifted for mbed
     static constexpr char REG_RAW_ANGLE = 0x0C;
@@ -34,7 +41,4 @@ private:
     static constexpr char REG_STATUS = 0x0B;
     static constexpr char REG_AGC = 0x1A;
 
-
-    uint16_t read16(char reg);
-    uint8_t read8(char reg);
 };
